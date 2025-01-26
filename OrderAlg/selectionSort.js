79a46301c8cf85fa2782bfaf3b4c66ec60e55bcf
Pedro Paulo <pedropscontatoprof@gmail.com>
2025-01-26 01:42:50 -0300

    Implementing the selection sort algorithm

--- OrderAlg/selectionSort.js
+++ OrderAlg/selectionSort.js
@@ -1,6 +1,15 @@
 const livros = require ("../listaLivros")
 const menorValor = require("../menorValor")
 
-for (let atual = 0; atual < livros.length;atual ++){
+for (let atual = 0; atual < livros.length -1 ;atual ++){
+    let menor = menorValor(livros, atual) ;
+    
+    let livroAtual = livros[atual];
+    let livroMenorPreco = livros[menor];
 
+    livros[atual] = livroMenorPreco;
+    livros[menor] = livroAtual;
+    
 }
+
+console.log(livros)
